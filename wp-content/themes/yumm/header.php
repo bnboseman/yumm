51036a95a2c2a40b750a8afda8304e7df76c7341
Nichole Boseman <bnboseman@gmail.com>
2016-03-18 00:11:33 -0400

    fixed typo in header file

--- wp-content/themes/yumm/header.php
+++ wp-content/themes/yumm/header.php
@@ -1,7 +1,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?phpbloginfo('pingback_url');?>">
-<link rel="stylesheet" type="text/css" href="<?phpbloginfo('stylesheet_url');?>" media="screen" />
+<link rel="pingback" href="<?php bloginfo('pingback_url');?>">
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url');?>" media="screen" />
 <?php wp_head(); ?>
 </head>
 
