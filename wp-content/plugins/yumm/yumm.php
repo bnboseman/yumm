7555015a5ede7ca9e4b73fefa0947cfcffbd36f0
EC2 Default User <ec2-user@ip-172-31-35-70.us-west-2.compute.internal>
2016-03-18 19:14:23 +0000

    fixing typon on yumm plugin

--- wp-content/plugins/yumm/yumm.php
+++ wp-content/plugins/yumm/yumm.php
@@ -95,32 +95,14 @@ class Yumm_Widget extends WP_Widget {
 	public function widget( $args, $instance ) {
 
 		echo $args['before_widget'];
-		if ( ! empty( $instance['title'] ) ) {
-			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		if ( ! empty( $instance['yumm_widget_title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['yumm_widget_title'] ). $args['after_title'];
 		}
-		echo __( 'Category', 'text_domain' );
+		echo '<h2>' . __( 'Recipes', 'text_domain' ) . '</h2>';
+		echo '<ul>';
+		wp_list_categories( ['taxonomy' => 'recipe-category','title_li' => null] );
+		echo '</ul>';
 		echo $args['after_widget'];
-	}
-	/**
-	 * Back-end widget form.
-	 *
-	 * @see WP_Widget::form()
-	 *
-	 * @param array $instance Previously saved values from database.
-	 */
-	public function form( $instance ) {
-		if ( isset( $instance[ 'title' ] ) ) {
-			$title = $instance[ 'title' ];
-		}
-		else {
-			$title = __( 'New title', 'text_domain' );
-		}
-		?>
-		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
-		</p>
-		<?php
 	}
 	/**
 	 * Sanitize widget form values as they are saved.
@@ -134,8 +116,7 @@ class Yumm_Widget extends WP_Widget {
 	 */
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['yumm_widget_title'] = ( ! empty( $new_instance['yumm_widget_title'] ) ) ? strip_tags( $new_instance['yumm_widget_title'] ) : '';
 		return $instance;
 	}
 }
-
