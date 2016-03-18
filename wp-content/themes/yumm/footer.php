43505c0da2a75660fdfaff69d7846e9f85caf989
Nichole Boseman <bnboseman@gmail.com>
2016-03-18 00:02:43 -0400

    adding header, footer and functions.php

--- wp-content/themes/yumm/footer.php
+++ wp-content/themes/yumm/footer.php
@@ -1,28 +1,4 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Yumm
- */
-
-?>
-
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'yumm' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'yumm' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'yumm' ), 'yumm', '<a href="http://nicholeboseman.com" rel="designer">Nichole Boseman</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
+</div>
 <?php wp_footer(); ?>
-
 </body>
 </html>
