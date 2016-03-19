d43050f3c60775ce667e1bf9a3918d71b9c017ff
Nichole Boseman <bnboseman@gmail.com>
2016-03-18 21:53:16 -0400

    added 404 page

---
+++ wp-content/themes/yumm/404.php
@@ -0,0 +1,13 @@
+<?php get_header() ?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <h1>404 Error</h1>
+        <p><em>The page could not be found.</em> Try to search for the page using the form below.</p>
+        <?php get_search_form(); ?>
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+<div class="sidebar">
+    <?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
