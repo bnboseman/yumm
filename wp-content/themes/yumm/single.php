22fedf79800fe0c0b9e4dad8e3003463ea8e9045
Nichole Boseman <bnboseman@gmail.com>
2016-03-19 09:54:33 -0400

    updated single to show different text if a post has one category or more

--- wp-content/themes/yumm/single.php
+++ wp-content/themes/yumm/single.php
@@ -14,8 +14,10 @@
 						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 						<?php the_post_thumbnail('medium'); ?>
 						</a>
-					<?php } ?>
-					<p><strong>Category:</strong><?php echo get_the_category_list(' '); ?></p>
+					<?php } 
+					$category_count = count(get_the_category());
+					?>
+					<p><strong><?php echo ($category_count > 1) ? 'Categories: ' :'Category: '?></strong><?php echo get_the_category_list(' '); ?></p>
 					<?php echo get_the_tag_list('<p><strong>Tags:</strong> ',', ','</p>'); ?>
 					<p><strong>Posted: </strong><?php the_date(); ?> by <strong><?php the_author_link(); ?></strong> </p>
 					<p><strong>Status:</strong> <?php echo ucfirst(get_post_status( )) ; ?></p>
