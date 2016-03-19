7411ae92b47a9c0fc54e6cb460492d7f045f0945
Nichole Boseman <bnboseman@gmail.com>
2016-03-18 22:05:12 -0400

    updated archive to show either category tag or date depending on which type of archive page it is'

--- wp-content/themes/yumm/archive.php
+++ wp-content/themes/yumm/archive.php
@@ -2,7 +2,18 @@
 get_header(); ?>
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
-		<h1><?php echo is_category() ? 'Category' : 'Tag'; ?>: <?php single_cat_title(); ?></h1>
+		<h1>
+        <?php if (is_tag()) {
+        	echo "Tag: ";
+        } elseif (is_category()) {
+        	echo "Category: ";
+        } elseif (is_date()) {
+        	echo "Date Archive";
+        } elseif (is_tax('recipe-category') ) {
+        	echo "Category: ";
+        }
+        
+        single_cat_title(); ?></h1>
 		<?php 
 		if (have_posts()) {
 			while ( have_posts()) {
