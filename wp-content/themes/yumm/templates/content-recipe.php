87a0c4f9e1a7e147509f453ebe504c7f03bc735e
EC2 Default User <ec2-user@ip-172-31-35-70.us-west-2.compute.internal>
2016-03-24 13:19:09 +0000

    updated look of images

--- wp-content/themes/yumm/templates/content-recipe.php
+++ wp-content/themes/yumm/templates/content-recipe.php
@@ -1,14 +1,19 @@
 <article>
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <div>
-    <?php
-    $category_count = count(get_the_category());
-    echo get_the_term_list( get_the_ID(), 'recipe-category', $category_count == 1 ?'<p><strong>Category: </strong>' : '<p><strong>Categories: </strong>', ', ', '</p>'); ?></div>
-    <?php
-    // show thumbnail of image if the recipe has one
-    if ( has_post_thumbnail() ) { ?>
-        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-            <?php the_post_thumbnail('medium'); ?>
-        </a>
-    <?php } ?>
+	<div class="recipelisting">
+		<?php
+		// show thumbnail of image if the recipe has one
+		if ( has_post_thumbnail() ) { ?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+				<?php the_post_thumbnail(); ?>
+        		</a>
+		<?php } ?>
+			<div>
+				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				<?php
+				$category_count = count(get_the_category());
+				echo get_the_term_list( get_the_ID(), 'recipe-category', $category_count == 1 ?
+				'<strong>Category: </strong>' :
+				'<strong>Categories: </strong>', ', ', ''); ?>
+			</div>
+		</div>
 </article>
